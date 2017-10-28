5e9b2b83b68472ebcff1dc13786defb78a7b2874
Awtian Akbar <me@awtian.com>
2017-10-28 07:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function destructivelyRemoveLastKitten() {
   return kittens.pop()
 }
 
-function destructiveltRemoveFirstKitten () {
+function destructivelyRemoveFirstKitten () {
   return kittens.shift()
 }
+
